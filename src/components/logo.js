43cbce4a5eda15282ff0499c094ea3ae40b4d871
Josh Zeilmann <jzeilmann93@gmail.com>
2018-08-06 19:06:25 -0500

    Create logo component

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from '../assets/residekc_logo.svg'
+
+export class Logo extends Component {
+  render() {
+    return (
+       <div className="container" id="body">
+         <img src={logo} alt="ResideKC Logo" className='logo' />
+       </div>
+    );
+  }
+}
