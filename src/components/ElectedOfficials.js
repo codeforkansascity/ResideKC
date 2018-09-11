394dc29c211228e601144e5e5c5c8b0be873ad70
Sandip Subedi <sandip.subedi@cerner.com>
2018-09-10 19:07:18 -0500

    Some cleanups

--- src/components/ElectedOfficials.js
+++ src/components/ElectedOfficials.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function ElectedOffialsInfo(props){
-    return(
+export default function ElectedOffialsInfo(props) {
+    return (
         <div className="otherInfo">
             <h2> Working on Elected official array. Will talk about API progression</h2>
         </div>
--- src/components/TrashDay.js
+++ src/components/TrashDay.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function TrashDay(props) {
-  return (
-      <div className="trashDay">
-          <h2>Your trash pickup day is {props.trashDay}</h2>
-      </div>
-  )
+
+    return (
+        <div className="trashDay">
+            <h2>Your trash pickup day is {props.trashDay}</h2>
+        </div>
+    )
 }
