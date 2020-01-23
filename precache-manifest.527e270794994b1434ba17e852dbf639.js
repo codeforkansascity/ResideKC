753fffa72ea6e64b30694a81b4259015a303c562
John <jdavidswift@gmail.com>
2020-01-23 09:27:38 -0600

    Updates

---
+++ precache-manifest.527e270794994b1434ba17e852dbf639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d895110f68c9d88ce3d83ff02b43cf4e",
+    "url": "/ResideKC/index.html"
+  },
+  {
+    "revision": "87c0e6712deff086c0dc",
+    "url": "/ResideKC/static/css/main.402f9250.chunk.css"
+  },
+  {
+    "revision": "6abda2894941d09ebe73",
+    "url": "/ResideKC/static/js/2.ca6f6d7d.chunk.js"
+  },
+  {
+    "revision": "87c0e6712deff086c0dc",
+    "url": "/ResideKC/static/js/main.e8f8eb49.chunk.js"
+  },
+  {
+    "revision": "28823894d9b09e61eb0e",
+    "url": "/ResideKC/static/js/runtime~main.202ee198.js"
+  },
+  {
+    "revision": "9602df153b02301535db928b7b7e9b37",
+    "url": "/ResideKC/static/media/residekc_logo.9602df15.svg"
+  }
+]);
